8141dd40bd662a6f144e90bfd537c764ee6ed19d
GIPNOGABA <144873162+GIPNOGABA@users.noreply.github.com>
2023-10-24 20:54:55 +0600

    Add files via upload

---
+++ Olimpiad/Pract 24.10.2023.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        int[] mas = new int[N + 1];
+        mas[1] = 0;
+        mas[2] = 0;
+        mas[3] = 1;
+        for (int i = 4; i < mas.Length; i++) mas[i] = mas[i / 2] + mas[i - (i / 2)];
+        Console.WriteLine(mas[N]);
+    }
+}
