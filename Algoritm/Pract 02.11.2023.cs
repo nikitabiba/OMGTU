7eac185e88a8ba67ffd895e1314601fb06d66968
GIPNOGABA <144873162+GIPNOGABA@users.noreply.github.com>
2023-11-02 23:30:30 +0600

    Add files via upload

---
+++ Algoritm/Pract 02.11.2023.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int kol = Convert.ToInt32(Console.ReadLine()), shag = Convert.ToInt32(Console.ReadLine()), counter = 0, count = 0;
+        int[] mas = new int[kol];
+        while (counter < kol - 1) for (int i = 0; i < kol; i++)
+            {
+                if (mas[i] != '-') count++;
+                if (count == shag && mas[i] != '-')
+                {
+                    mas[i] = '-';
+                    counter++;
+                    count = 0;
+                }
+            }
+        if (mas.Contains(0)) Console.WriteLine(Array.IndexOf(mas, 0) + 1);
+    }
+}
