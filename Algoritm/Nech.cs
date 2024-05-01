8e21a90e27b40835846b291debfcea8ad855344e
GIPNOGABA <144873162+GIPNOGABA@users.noreply.github.com>
2024-05-01 20:20:16 +0600

    Add files via upload

---
+++ Algoritm/Nech.cs
@@ -0,0 +1,13 @@
+﻿int count = 0;
+using (StreamReader sr = new StreamReader(@"C:\Users\csr10\OneDrive\Рабочий стол\Work\Cishka\Project_1\File.txt"))
+{
+    string a;
+    while ((a = sr.ReadLine()) != null)
+    {
+        if ((int.Parse(Convert.ToString(a[a.Length - 1])) % 2) == 1)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count * (count - 1) / 2);
+}
