356533cae70dd0694f220f5c18abbfbc8ef66f98
GIPNOGABA <144873162+GIPNOGABA@users.noreply.github.com>
2024-05-01 20:23:35 +0600

    Add files via upload

---
+++ Algoritm/LINQ_1.cs
@@ -0,0 +1,14 @@
+﻿List<string> stringList = new List<string>() { "Abra", "Kadabra", "Biba", "Boba", "Pupa", "Lupa", "Aboba" };
+var selectedStrings = from str in stringList
+                    where str.Length % 2 == 0
+                    select str;
+foreach (string str in selectedStrings) Console.WriteLine(str);
+for (int i = stringList.Count - 1; i >= 0; i--)
+{
+    if (i % 2 == 1)
+    {
+        stringList.RemoveAt(i);
+    }
+}
+Console.WriteLine();
+foreach (string str in selectedStrings) Console.WriteLine(str);
